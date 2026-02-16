398c7613212848206ef1c4701aae28a113ab7acd
“PedroFabian43“ <pmfabiangarcia@gmail.com>
2026-02-16 20:23:21 +0100

    Hemos añadido la base de datos de mssql server

---
+++ mainpruebasqlserver.py
@@ -0,0 +1,12 @@
+import mssql_python
+
+connection = mssql_python.connect('Server=sqlpaco3430.database.windows.net;Database=AZURETAJAMAR;Encrypt=yes;UID=adminsql;PWD=Admin123;TrustServerCertificate=yes')
+print("Funciona SQL Server")
+sql = "select * from EMP"
+cursor = connection.cursor()
+cursor.execute(sql)
+for row in cursor:
+    print(f"{row[1]}")
+cursor.close()
+connection.close()
+print("Fin de programa")
